070bddbf79a5d87f7076ec7747b4012b20708b38
Raidline <pedrofilipefialho19@gmail.com>
2022-09-12 08:05:17 +0100

    Injects thymeleaf into viewResolvers

--- src/main/java/com/jeroenreijn/examples/configuration/WebFluxConfig.java
+++ src/main/java/com/jeroenreijn/examples/configuration/WebFluxConfig.java
@@ -1,54 +1,53 @@
 package com.jeroenreijn.examples.configuration;
 
-import com.github.enpassant.ickenham.springmvc.IckenhamViewResolver;
 import com.jeroenreijn.examples.model.Presentation;
 import com.jeroenreijn.examples.repository.InMemoryPresentationsRepository;
 import com.jeroenreijn.examples.repository.PresentationsRepository;
 import com.jeroenreijn.examples.view.HtmlFlowViewResolver;
 import com.jeroenreijn.examples.view.KotlinxHtmlViewResolver;
-import com.jeroenreijn.examples.view.LiqpView;
 import com.jeroenreijn.examples.view.LiqpViewResolver;
 import com.jeroenreijn.examples.view.RockerViewResolver;
 import com.jeroenreijn.examples.view.TrimouViewResolver;
 import com.jeroenreijn.examples.view.response.ReactiveResponseWriter;
 import com.jeroenreijn.examples.view.response.ReactiveResponseWriterImpl;
-import org.rythmengine.spring.web.RythmConfigurer;
-import org.rythmengine.spring.web.RythmViewResolver;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.ViewResolverRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.view.ViewResolver;
-import org.thymeleaf.spring5.SpringWebFluxTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableWebFlux
-@ComponentScan(basePackages = { "com.jeroenreijn.examples.controller", "com.jeroenreijn.examples.factory" })
 public class WebFluxConfig implements ApplicationContextAware, WebFluxConfigurer {
 	private ApplicationContext applicationContext;
 
+	//TODO ADD HANDLEBARS AGAIN BUT ADD CUSTOM CODE
+	//TODO SEE SELF PROJECT
 	@Override
 	public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
 	}
-
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.viewResolver(this.rockerViewResolver());
+		registry.viewResolver(this.htmlFlowViewResolver());
+		registry.viewResolver(this.trimouViewResolver());
+		registry.viewResolver(this.liqpViewResolver());
+		registry.viewResolver(this.kotlinxHtmlViewResolver());
+		registry.viewResolver(applicationContext.getBean(ThymeleafReactiveViewResolver.class));
+	}
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/robots.txt").addResourceLocations("/robots.txt");
--- src/main/java/com/jeroenreijn/examples/controller/PresentationsController.java
+++ src/main/java/com/jeroenreijn/examples/controller/PresentationsController.java
@@ -5,7 +5,9 @@ import com.jeroenreijn.examples.model.Presentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -44,13 +46,14 @@ public class PresentationsController {
 		return "index-" + template;
 	}
 	
-	@RequestMapping(value = "/async/{template}", method = RequestMethod.GET)
+	@GetMapping("/async/{template}")
 	public String showListWebflux(@PathVariable(value = "template") final String template,
-						   final ModelMap model) {
+						   final Model model) {
 		System.out.println("Currently benchmarking : " + template);
 		final Flux<Presentation> presentations = presentationsService.findAllReactive().delayElements(Duration.ofSeconds(1L));
 		IReactiveDataDriverContextVariable rx = new ReactiveDataDriverContextVariable(presentations);
-		model.addAttribute("presentations", rx);
+		model.addAttribute("reactivedata", rx);
+		model.addAttribute("presentations", presentations);
 		model.addAttribute("i18n", new i18nLayout(messageSource));
 		
 		return "index-" + template;
